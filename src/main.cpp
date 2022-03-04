8edf4339fa4b0db08a60cd91aa837dd001a6ab3d
XiotSamuel <samuelmoktm@gmail.com>
2022-03-04 18:13:33 +0800

    testing on the sender

--- src/main.cpp
+++ src/main.cpp
@@ -13,7 +13,7 @@
 #include <RHSoftwareSPI.h>
 
 #define CLIENT_ADDRESS 1
-#define SERVER_ADDRESS 2
+#define SERVER_ADDRESS 5
 
 // Singleton instance of the radio driver
 //RH_RF22 driver;
@@ -27,39 +27,25 @@ RHReliableDatagram manager(driver, CLIENT_ADDRESS);
 
 void setup() 
 {
-  Serial.begin(9600);
+  Serial.begin(115200);
+
+  spi.setPins(12,13,14);
+
   if (!manager.init())
     Serial.println("init failed");
   // Defaults after init are 434.0MHz, 0.05MHz AFC pull-in, modulation FSK_Rb2_4Fd36
 }
 
-uint8_t data[] = "Hello World!";
+uint8_t data[] = "FFFFFFFF";
 // Dont put this on the stack:
 uint8_t buf[RH_RF22_MAX_MESSAGE_LEN];
 
 void loop()
 {
-  Serial.println("Sending to rf22_reliable_datagram_server");
-    
+  Serial.println("Sending data");
+  driver.send(data, sizeof(data));
   // Send a message to manager_server
-  if (manager.sendtoWait(data, sizeof(data), SERVER_ADDRESS))
-  {
-    // Now wait for a reply from the server
-    uint8_t len = sizeof(buf);
-    uint8_t from;   
-    if (manager.recvfromAckTimeout(buf, &len, 2000, &from))
-    {
-      Serial.print("got reply from : 0x");
-      Serial.print(from, HEX);
-      Serial.print(": ");
-      Serial.println((char*)buf);
-    }
-    else
-    {
-      Serial.println("No reply, is rf22_reliable_datagram_server running?");
-    }
-  }
-  else
-    Serial.println("sendtoWait failed");
-  delay(500);
+
+  driver.waitPacketSent();
+  delay(1000);
 }
